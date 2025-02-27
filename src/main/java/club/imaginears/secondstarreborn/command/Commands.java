8a7c257531ca74918dc0c77330d99f72a5025356
Corey Clark <Clarkcj12@users.noreply.github.com>
2025-02-27 11:04:22 -0500

    Add Commands class with initialization method
    Introduce `Commands` class to handle command-related functionality. Includes an `init` method to set up commands using `CommandManager` and `SecondStar` instance. Currently serves as a placeholder for future implementations.

---
+++ src/main/java/club/imaginears/secondstarreborn/command/Commands.java
@@ -0,0 +1,9 @@
+package club.imaginears.secondstarreborn.command;
+
+import com.velocitypowered.api.command.CommandManager;
+import club.imaginears.secondstarreborn.SecondStar;
+
+public class Commands {
+    public static void init(CommandManager manager, SecondStar instance) {
+
+    }
