d202d8fc267855c8b8ba8ea1dd5d86415f5ab1df
Corey Clark <Clarkcj12@users.noreply.github.com>
2025-03-01 02:06:29 -0500

    Add CreateServerPacket class for server creation messaging
    This class facilitates the creation and transmission of server-related data using JSON, providing constructors for various initialization methods. It also includes a method to serialize server details into JSON format, ensuring consistent messaging structures.

---
+++ src/main/java/club/imaginears/secondstarreborn/messages/packets/CreateServerPacket.java
@@ -0,0 +1,45 @@
+package club.imaginears.secondstarreborn.messages.packets;
+
+import club.imaginears.secondstarreborn.handlers.Server;
+import com.google.gson.JsonObject;
+import lombok.Getter;
+
+public class CreateServerPacket extends MQPacket {
+    @Getter private final String name, address, type;
+    @Getter
+    private final boolean park;
+
+    public CreateServerPacket(JsonObject object) {
+        super(PacketID.Global.CREATESERVER.getId(), object);
+        this.name = object.get("name").getAsString();
+        this.address = object.get("address").getAsString();
+        this.type = object.get("type").getAsString();
+        this.park = object.get("park").getAsBoolean();
+    }
+
+    public CreateServerPacket(String name, String address, String type, boolean park) {
+        super(PacketID.Global.CREATESERVER.getId(), null);
+        this.name = name;
+        this.address = address;
+        this.type = type;
+        this.park = park;
+    }
+
+    public CreateServerPacket(Server s) {
+        super(PacketID.Global.CREATESERVER.getId(), null);
+        this.name = s.getName();
+        this.address = s.getAddress();
+        this.type = s.getServerType();
+        this.park = s.isPark();
+    }
+
+    @Override
+    public JsonObject getJSON() {
+        JsonObject object = getBaseJSON();
+        object.addProperty("name", name);
+        object.addProperty("address", address);
+        object.addProperty("type", type);
+        object.addProperty("park", park);
+        return object;
+    }
+}
