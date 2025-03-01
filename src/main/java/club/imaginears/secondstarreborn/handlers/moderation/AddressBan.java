032eb74b2e83fbb5d3cc5b94164ca144f32465ac
Corey Clark <Clarkcj12@users.noreply.github.com>
2025-02-28 21:27:14 -0500

    Add AddressBan class for managing banned addresses
    Introduces a new class, `AddressBan`, to encapsulate details about banned addresses, including the address, reason, and source. This will facilitate cleaner handling of address bans in the moderation module.

---
+++ src/main/java/club/imaginears/secondstarreborn/handlers/moderation/AddressBan.java
@@ -0,0 +1,11 @@
+package club.imaginears.secondstarreborn.handlers.moderation;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+public class AddressBan {
+    @Getter private final String address;
+    @Getter private final String reason;
+    @Getter private final String source;
+}
