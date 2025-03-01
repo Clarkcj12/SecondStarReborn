75fb4c0bd4f62ee1c045b1377f23b79103837440
Corey Clark <Clarkcj12@users.noreply.github.com>
2025-03-01 02:08:13 -0500

    Add new packet classes for DeleteServer, DM, and ChatMute
    Introduce `DeleteServerPacket`, `DMPacket`, and `ChatMutePacket` to handle respective messaging functionalities. These classes extend `MQPacket` and provide serialization and deserialization through JSON, enabling structured communication across the system.

---
+++ src/main/java/club/imaginears/secondstarreborn/messages/packets/DMPacket.java
@@ -0,0 +1,65 @@
+package club.imaginears.secondstarreborn.messages.packets;
+
+import club.imaginears.secondstarreborn.handlers.Rank;
+import com.google.gson.JsonObject;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+public class DMPacket extends MQPacket {
+    private final String from, to, message, channel, command;
+    private final UUID fromUUID, toUUID;
+    private final boolean initialSend;
+    private final Rank rank;
+
+    public DMPacket(JsonObject object) {
+        super(PacketID.Global.DM.getId(), object);
+        this.from = object.get("from").getAsString();
+        this.to = object.get("to").getAsString();
+        if (object.has("fromUUID")) {
+            this.fromUUID = UUID.fromString(object.get("fromUUID").getAsString());
+        } else {
+            this.fromUUID = null;
+        }
+        if (object.has("toUUID")) {
+            this.toUUID = UUID.fromString(object.get("toUUID").getAsString());
+        } else {
+            this.toUUID = null;
+        }
+        this.message = object.get("message").getAsString();
+        this.channel = object.has("channel") ? object.get("channel").getAsString() : "ParkChat";
+        this.command = object.has("command") ? object.get("command").getAsString() : "msg";
+        this.sendingProxy = UUID.fromString(object.get("sendingProxy").getAsString());
+        this.initialSend = object.get("initialSend").getAsBoolean();
+        this.rank = Rank.fromString(object.get("rank").getAsString());
+    }
+
+    public DMPacket(String from, String to, String message, String channel, String command, UUID fromUUID, UUID toUUID, UUID sendingProxy, boolean initialSend, Rank rank) {
+        super(PacketID.Global.DM.getId(), null);
+        this.from = from;
+        this.to = to;
+        this.message = message;
+        this.channel = channel;
+        this.command = command;
+        this.fromUUID = fromUUID;
+        this.toUUID = toUUID;
+        this.sendingProxy = sendingProxy;
+        this.initialSend = initialSend;
+        this.rank = rank;
+    }
+
+    @Override
+    public JsonObject getJSON() {
+        JsonObject object = getBaseJSON();
+        object.addProperty("from", from);
+        object.addProperty("to", to);
+        if (fromUUID != null) object.addProperty("fromUUID", fromUUID.toString());
+        if (toUUID != null) object.addProperty("toUUID", toUUID.toString());
+        object.addProperty("message", message);
+        object.addProperty("sendingProxy", sendingProxy.toString());
+        object.addProperty("initialSend", initialSend);
+        object.addProperty("rank", rank.getDBName());
+        return object;
+    }
+}
---
+++ src/main/java/club/imaginears/secondstarreborn/messages/packets/DeleteServerPacket.java
@@ -0,0 +1,26 @@
+package club.imaginears.secondstarreborn.messages.packets;
+
+import com.google.gson.JsonObject;
+import lombok.Getter;
+
+public class DeleteServerPacket extends MQPacket {
+    @Getter
+    private final String name;
+
+    public DeleteServerPacket(JsonObject object) {
+        super(PacketID.Global.DELETESERVER.getId(), object);
+        this.name = object.get("name").getAsString();
+    }
+
+    public DeleteServerPacket(String name) {
+        super(PacketID.Global.DELETESERVER.getId(), null);
+        this.name = name;
+    }
+
+    @Override
+    public JsonObject getJSON() {
+        JsonObject object = getBaseJSON();
+        object.addProperty("name", name);
+        return object;
+    }
+}
