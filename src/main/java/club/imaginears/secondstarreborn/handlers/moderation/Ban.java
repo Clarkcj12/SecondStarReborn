fad4d7640167ca46eb3e59a149532f289cfb5857
Corey Clark <Clarkcj12@users.noreply.github.com>
2025-02-28 21:26:28 -0500

    Add Kick and Ban classes for moderation handling
    Introduced `Kick` and `Ban` classes to manage user moderation actions. These classes encapsulate data for kicks and bans, including reasons, source, and timing information, streamlining the implementation of moderation features.

---
+++ src/main/java/club/imaginears/secondstarreborn/handlers/moderation/Ban.java
@@ -0,0 +1,34 @@
+package club.imaginears.secondstarreborn.handlers.moderation;
+
+import lombok.Getter;
+
+import java.util.UUID;
+
+public class Ban {
+    private final UUID uuid;
+    @Getter private final String name;
+    @Getter private final boolean permanent;
+    @Getter private final long created;
+    @Getter private final long expires;
+    @Getter private final String reason;
+    @Getter
+    private final String source;
+
+    public Ban(UUID uuid, String name, boolean permanent, long expires, String reason, String source) {
+        this(uuid, name, permanent, System.currentTimeMillis(), expires, reason, source);
+    }
+
+    public Ban(UUID uuid, String name, boolean permanent, long created, long expires, String reason, String source) {
+        this.uuid = uuid;
+        this.name = name;
+        this.permanent = permanent;
+        this.created = created;
+        this.expires = expires;
+        this.reason = reason;
+        this.source = source;
+    }
+
+    public UUID getUniqueId() {
+        return uuid;
+    }
+}
