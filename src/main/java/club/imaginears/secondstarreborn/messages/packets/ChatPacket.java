7e780f9c02b9bd81d3af879da4822fbdba043f31
Corey Clark <Clarkcj12@users.noreply.github.com>
2025-03-01 02:10:05 -0500

    Add EmptyServerPacket, ChatPacket, and EmojiUtil classes
    Introduced EmptyServerPacket and ChatPacket for handling server and chat messaging, respectively. Added EmojiUtil utility to process emoji translations in messages with rank-based access and tab-completion support. These changes enhance messaging functionality within the system.

---
+++ src/main/java/club/imaginears/secondstarreborn/messages/packets/ChatPacket.java
@@ -0,0 +1,47 @@
+package club.imaginears.secondstarreborn.messages.packets;
+
+import club.imaginears.secondstarreborn.handlers.Rank;
+import com.google.gson.JsonObject;
+import lombok.Getter;
+import net.kyori.adventure.text.serializer.ComponentSerializer;
+
+import java.util.UUID;
+
+public class ChatPacket extends MQPacket {
+    @Getter private final UUID sender;
+    @Getter private final Rank rank;
+    @Getter
+    private final String serializedMessage;
+    // ParkChat, or server name
+    @Getter private final String channel;
+
+    public ChatPacket(JsonObject object) {
+        super(PacketID.Global.CHAT.getId(), object);
+        this.sender = UUID.fromString(object.get("sender").getAsString());
+        this.rank = Rank.fromString(object.get("rank").getAsString());
+        this.serializedMessage = object.get("serializedMessage").getAsString();
+        this.channel = object.get("channel").getAsString();
+    }
+
+    public ChatPacket(UUID sender, Rank rank, BaseComponent[] message, String channel) {
+        super(PacketID.Global.CHAT.getId(), null);
+        this.sender = sender;
+        this.rank = rank;
+        this.serializedMessage = ComponentSerializer.toString(message);
+        this.channel = channel;
+    }
+
+    public BaseComponent[] getMessage() {
+        return ComponentSerializer.parse(serializedMessage);
+    }
+
+    @Override
+    public JsonObject getJSON() {
+        JsonObject object = getBaseJSON();
+        object.addProperty("sender", sender.toString());
+        object.addProperty("rank", rank.getDBName());
+        object.addProperty("serializedMessage", serializedMessage);
+        object.addProperty("channel", channel);
+        return object;
+    }
+}
