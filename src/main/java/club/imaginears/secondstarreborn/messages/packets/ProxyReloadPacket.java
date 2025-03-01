a69e78e8f692b7f03250e284dba3fd28e0071042
Corey Clark <Clarkcj12@users.noreply.github.com>
2025-03-01 02:21:25 -0500

    Add ProxyReloadPacket implementation
    Introduce ProxyReloadPacket class extending MQPacket to handle proxy reload functionality. Includes constructors for JSON input and default creation, and overrides getJSON method to return base JSON structure.

---
+++ src/main/java/club/imaginears/secondstarreborn/messages/packets/ProxyReloadPacket.java
@@ -0,0 +1,19 @@
+package club.imaginears.secondstarreborn.messages.packets;
+
+import com.google.gson.JsonObject;
+
+public class ProxyReloadPacket extends MQPacket {
+
+    public ProxyReloadPacket(JsonObject object) {
+        super(PacketID.Global.PROXYRELOAD.getId(), object);
+    }
+
+    public ProxyReloadPacket() {
+        super(PacketID.Global.PROXYRELOAD.getId(), null);
+    }
+
+    @Override
+    public JsonObject getJSON() {
+        return getBaseJSON();
+    }
+}
