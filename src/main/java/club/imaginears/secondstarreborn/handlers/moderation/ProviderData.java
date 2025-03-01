f05b5e774e46fe1ae21dfc1fe887e71d204abb37
Corey Clark <Clarkcj12@users.noreply.github.com>
2025-02-28 21:23:04 -0500

    Add ProviderData class for storing and formatting provider info
    Introduces a new class `ProviderData` with fields for ISP, country, region, region name, and timezone. Includes a `toString` method for formatted string representation of the data. Uses Lombok annotations for boilerplate code reduction.

---
+++ src/main/java/club/imaginears/secondstarreborn/handlers/moderation/ProviderData.java
@@ -0,0 +1,16 @@
+package club.imaginears.secondstarreborn.handlers.moderation;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+public class ProviderData {
+    @Getter
+    private final String isp, country, region, regionName, timezone;
+
+    @Override
+    public String toString() {
+        return "isp:" + isp + ";country:" + country + ";region:" + region + ";regionName:" + regionName +
+                ";timezone:" + timezone;
+    }
+}
