069e4742f14cc22c22758b35c8ddbed8205c27a3
Corey Clark <Clarkcj12@users.noreply.github.com>
2025-03-01 02:16:38 -0500

    Add MentionPacket class for handling player mentions
    This commit introduces the MentionPacket class to manage player mentions in messages. It supports initialization with a list of UUIDs or a single UUID and serializes the data into JSON format. This is a foundational step for enabling mention-related functionality in the system.

---
+++ src/main/java/club/imaginears/secondstarreborn/messages/packets/MentionPacket.java
@@ -0,0 +1,51 @@
+package club.imaginears.secondstarreborn.messages.packets;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+public class MentionPacket extends MQPacket {
+    @Getter
+    private final List<UUID> players;
+
+    public MentionPacket(JsonObject object) {
+        super(PacketID.Global.MENTION.getId(), object);
+        this.players = new ArrayList<>();
+        if (object.has("players")) {
+            JsonArray array = object.get("players").getAsJsonArray();
+            for (JsonElement e : array) {
+                players.add(UUID.fromString(e.getAsString()));
+            }
+        } else {
+            players.add(UUID.fromString(object.get("uuid").getAsString()));
+        }
+    }
+
+    public MentionPacket(List<UUID> players) {
+        super(PacketID.Global.MENTION.getId(), null);
+        this.players = players;
+    }
+
+    public MentionPacket(UUID... players) {
+        super(PacketID.Global.MENTION.getId(), null);
+        this.players = new ArrayList<>(Arrays.asList(players));
+    }
+
+    @Override
+    public JsonObject getJSON() {
+        JsonObject object = getBaseJSON();
+        object.addProperty("uuid", players.get(0).toString());
+
+        JsonArray players = new JsonArray();
+        this.players.forEach(p -> players.add(p.toString()));
+        object.add("players", players);
+
+        return object;
+    }
+}
