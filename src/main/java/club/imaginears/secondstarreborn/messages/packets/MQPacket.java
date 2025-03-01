b1e802278bff1495440dac517fa4e3fd1344b9c0
Corey Clark <Clarkcj12@users.noreply.github.com>
2025-03-01 01:39:40 -0500

    Add abstract MQPacket class for message handling
    Introduce a base abstract class for message packets, including JSON serialization and byte conversion functionality. This will serve as a foundation for handling inter-proxy communication packets.

---
+++ src/main/java/club/imaginears/secondstarreborn/messages/packets/MQPacket.java
@@ -0,0 +1,43 @@
+package club.imaginears.secondstarreborn.messages.packets;
+
+import club.imaginears.secondstarreborn.SecondStar;
+import com.google.gson.JsonObject;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+public abstract class MQPacket {
+    @Getter
+    private int id;
+    @Getter protected UUID sendingProxy = null;
+
+    protected MQPacket(int id, JsonObject object) {
+        this.id = id;
+        if (object != null && object.get("id").getAsInt() != id)
+            throw new IllegalArgumentException("Packet id does not match!");
+    }
+
+    public MQPacket(JsonObject obj) {
+    }
+
+    public abstract JsonObject getJSON();
+
+    protected JsonObject getBaseJSON() {
+        JsonObject object = new JsonObject();
+        object.addProperty("id", id);
+        if (sendingProxy != null) object.addProperty("proxyID", sendingProxy.toString());
+        return object;
+    }
+
+    public byte[] toBytes() {
+        JsonObject obj = getJSON();
+        if (obj != null) return obj.toString().getBytes(StandardCharsets.UTF_8);
+        else {
+            SecondStar.getProxyServer().getLogger().severe("JSON Object for packet is null! " + this.getClass().getName());
+            return new byte[0];
+        }
+    }
+}
